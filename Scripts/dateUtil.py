a0cc8275fc14827a98c1e6158036b2ad4239d403
FacundoPazos <facundopazosm@gmail.com>
2016-11-24 18:55:41 -0300

    Logica video match converter

--- Scripts/dateUtil.py
+++ Scripts/dateUtil.py
@@ -21,7 +21,8 @@ def get_current_time_int():
     return int(datetime.now().strftime('%H%M'))
 
 def get_time(date_to_convert):
-    date_time = date_to_convert.split('_')[1]
+    date_aux = date_to_convert.split('_')[1]
+    date_time = date_aux.split('.')[0]
     return date_time.split('-')[0] + date_time.split('-')[1] + date_time.split('-')[2]
 
 
@@ -46,6 +47,25 @@ def get_time_subtraction(date_to_convert, time_recording):
      
     return hours + minuts + seconds
 
+def get_time_subtr(time_start, time_recording):
+    seconds_start = time_start[4:6]
+    minutes_start = int(time_start[2:4])
+    hours_start = int(time_start[0:2])
+    minuts_int = (minutes_start-time_recording)%60
+
+    if minuts_int>minutes_start:
+        hours_start=hours_start-1
+        if hours_start<10:
+            hours = '0'+str(hours_start)
+        else:
+            hours = str(hours_start)   
+    
+    if minuts_int<10:
+        minuts = '0'+str(minuts_int)
+    else:
+        minuts = str(minuts_int)
+     
+    return hours + minuts + seconds_start
 
 def get_time_adition(date_to_convert, time_recording):
     date_time = date_to_convert.split('_')[1]
@@ -67,3 +87,38 @@ def get_time_adition(date_to_convert, time_recording):
         minuts = str(minuts_int)
      
     return hours + minuts + seconds
+
+def get_time_adi(time_start, time_recording):
+    seconds_start = time_start[4:6]
+    minutes_start = int(time_start[2:4])
+    hours_start = int(time_start[0:2])
+    minuts_int = (minutes_start+time_recording)%60
+
+    if minuts_int<minutes_start:
+        hours_start=hours_start+1
+        if hours_start<10:
+            hours = '0'+str(hours_start)
+        else:
+            hours = str(hours_start)   
+    
+    if minuts_int<10:
+        minuts = '0'+str(minuts_int)
+    else:
+        minuts = str(minuts_int)
+     
+    return hours + minuts + seconds_start
+
+def get_seconds_cut(time_first_video, time_match):
+    second_first_video_str = time_first_video[4:6]
+    minutes_first_video_str = time_first_video[2:4]
+    hours_first_video_str = time_first_video[0:2]
+    seconds_first_video = int(second_first_video_str) + int(minutes_first_video_str)*60 + int(hours_first_video_str)*3600
+
+    second_time_match_str = time_match[4:6]
+    minutes_time_match_str = time_match[2:4]
+    hours_time_match_str = time_match[0:2]
+    seconds_match = int(second_time_match_str) + int(minutes_time_match_str)*60 + int(hours_time_match_str)*3600
+
+    return seconds_match - seconds_first_video
+
+
