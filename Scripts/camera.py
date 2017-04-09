82dfe39666a3dbf6053c78a87346b8808ace38d1
= <=>
2017-04-09 12:56:37 +0000

    cambios en audio

--- Scripts/camera.py
+++ Scripts/camera.py
@@ -5,6 +5,7 @@ import os
 
 #Constantes de la base de datos
 PATH_VIDEO_LOCALIZATION = '/home/pi/ViviFutbolLocal/Videos/'
+PATH_AUDIO_LOCALIZATION = '/home/pi/ViviFutbolLocal/Audios/'
 TIME_RECORDING_VIDEO=15
 START_RECORDING_TIME=900
 FINISH_RECORDING_TIME=1823
@@ -13,7 +14,6 @@ camera = PiCamera()
 
 try:
     camera.resolution = (640,480)
-    #camera.resolution = (1280,960)
     camera.framerate = 30
     
     total_video = 0
@@ -22,13 +22,19 @@ try:
         while (current_time >= START_RECORDING_TIME) & (current_time <= FINISH_RECORDING_TIME):
             # Obtengo el path de donde se va a crear el video        
             video_path = PATH_VIDEO_LOCALIZATION + get_current_short_date_str() + '/'
+            audio_path = PATH_AUDIO_LOCALIZATION + get_current_short_date_str() + '/'
 
             if not os.path.exists(video_path):
                 # En caso de no existir el directorio lo creo
                 os.makedirs(video_path)
 
+            if not os.path.exists(audio_path):
+                # En caso de no existir el directorio lo creo
+                os.makedirs(audio_path)
+
             camera.start_recording(video_path + get_current_date_str() + '.h264', format='h264', intra_period=1)
-            camera.wait_recording(TIME_RECORDING_VIDEO)
+            os.system('arecord -D plughw:1 --duration=' + str(TIME_RECORDING_VIDEO) + ' -f cd -vv ' + audio_path + get_current_date_str() + '.wav')
+            #camera.wait_recording(TIME_RECORDING_VIDEO)
             camera.stop_recording()
             current_time = get_current_time_int()
         total_video = total_video+1
--- Scripts/videoConverter.py
+++ Scripts/videoConverter.py
@@ -4,28 +4,40 @@ from fileUtil import get_h264_files_in_directory, delete_file, newest_h264_in_di
 from dateUtil import get_current_short_date_str
 
 # Constantes de la base de datos
-PATH_VIDEO_LOCALIZATION = '/home/pi/ViviFutbolLocal/VideosEXPO/'
-SECONDS_WAITING_FOR_CONVERT_VIDEO=30
+PATH_VIDEO_LOCALIZATION = '/home/pi/ViviFutbolLocal/Videos/'
+PATH_AUDIO_LOCALIZATION = '/home/pi/ViviFutbolLocal/Audios/'
+SECONDS_WAITING_FOR_CONVERT_VIDEO=15
 last_newest_file = ''
 
 try:
     var = 0
     while var < 10 :
         video_path = PATH_VIDEO_LOCALIZATION + get_current_short_date_str()
+        audio_path = PATH_AUDIO_LOCALIZATION + get_current_short_date_str()
         video_path_mp4 = video_path + '/mp4/'
         file_array = get_h264_files_in_directory(video_path)
         newest_file = newest_h264_in_directory(video_path + '/')
+        
         if len(file_array) > 0 :
             for file_name in file_array:
                 if (file_name != newest_file) | (newest_file == last_newest_file) :
                     if not os.path.exists(video_path_mp4):
                         # En caso de no existir el directorio lo creo
                         os.makedirs(video_path_mp4)
+                        
+                    aux_file_name = file_name.replace(video_path, video_path_mp4)
+                    aux_file_name = aux_file_name.replace('.h264', '_aux.mp4')
+                    new_file_name = aux_file_name.replace('_aux.mp4', '.mp4')
+
+                    audio_file_name = file_name.replace("Videos", "Audios")
+                    audio_file_name = audio_file_name.replace(".h264", ".wav")
                     
-                    file_new_name = file_name.replace(video_path, video_path_mp4)
-                    file_new_name = file_new_name.replace('.h264', '.mp4')
-                    os.system('MP4Box -add '+ file_name +':fps=30 '+file_new_name)
+                    os.system('MP4Box -add '+ file_name +':fps=30 '+ aux_file_name)
+                    os.system('avconv -i ' + aux_file_name + ' -i ' + audio_file_name + ' -vcodec copy -strict experimental -shortest ' + new_file_name)
+
                     delete_file(file_name)
+                    delete_file(aux_file_name)
+                    delete_file(audio_file_name)
         var = var + 1
         time.sleep(SECONDS_WAITING_FOR_CONVERT_VIDEO)
         last_newest_file = newest_file
