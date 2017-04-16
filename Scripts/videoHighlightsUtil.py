154aacc893f8aa5bbbfa6e007e78abacf467e70f
= <=>
2017-04-15 22:25:07 -0300

    cambios jugadas destacadas

--- Scripts/videoHighlightsUtil.py
+++ Scripts/videoHighlightsUtil.py
@@ -20,10 +20,11 @@ find_video = False
 concat_string = ''
 last_mark_date = ''
 video_path = ''
+
+
 for row in marks:
 
     mark_date = row.split('_')[0]
-
     if mark_date != last_mark_date:
         find_video = False
         i=0
@@ -38,7 +39,8 @@ for row in marks:
             os.makedirs(new_video_path)
 
     while ((find_video == False) and (i+1<len(file_array))):
-        if (int(get_time_subtr(get_time(row), TIME_BEFORE)) > int(get_time(file_array[i]))) and (int(get_time_subtr(get_time(row), TIME_BEFORE)) < int(get_time(file_array[i+1]))) and (int(get_time_adi(get_time(row), TIME_AFTER)) < int(get_time(file_array[i+1]))):
+        if (int(get_time_subtr(get_time(row), TIME_BEFORE)) > int(get_time(file_array[i]))) and (int(get_time_subtr(get_time(row), TIME_BEFORE)) < int(get_time(file_array[i+1]))) and (int(get_time_subtr(get_time(row), TIME_AFTER)) < int(get_time(file_array[i+1]))):
+            #if (int(get_time_subtr(get_time(row), TIME_BEFORE)) > int(get_time(file_array[i]))) and (int(get_time_subtr(get_time(row), TIME_BEFORE)) < int(get_time(file_array[i+1]))) and (int(get_time_adi(get_time(row), TIME_AFTER)) < int(get_time(file_array[i+1]))):
             find_video = True
             file_array_highlight.append(file_array[i])
              
@@ -46,17 +48,17 @@ for row in marks:
             seconds_finish_cut = get_seconds_cut(get_time(file_array[i]), get_time_adi(get_time(row), TIME_AFTER))
             os.system("MP4Box -splitx " + "2" + ":" + "4 " + file_array[i] + " -out " + new_video_path + HIGHLIGHT_NAME + row + ".mp4")
         else:
-            if (int(get_time_subtr(get_time(row), TIME_BEFORE)) > int(get_time(file_array[i]))) and (int(get_time_subtr(get_time(row), TIME_BEFORE)) < int(get_time(file_array[i+1]))) and (int(get_time_adi(get_time(row), TIME_AFTER)) > int(get_time(file_array[i+1]))):
+            if (int(get_time_subtr(get_time(row), TIME_BEFORE)) > int(get_time(file_array[i]))) and (int(get_time_subtr(get_time(row), TIME_BEFORE)) < int(get_time(file_array[i+1]))) and (int(get_time_subtr(get_time(row), TIME_AFTER)) > int(get_time(file_array[i+1]))):
                 find_video = True
                 file_array_highlight.append(file_array[i])
                 file_array_highlight.append(file_array[i+1])
 
                 for file_name in file_array_highlight:
                     concat_string = concat_string + " -cat " + file_name
-                os.system("MP4Box" + concat_string + " -new " + new_video_path + COMPLETE_NAME + row + ".mp4")
+                #os.system("MP4Box" + concat_string + " -new " + new_video_path + COMPLETE_NAME + row + ".mp4")
 
                 seconds_start_cut = get_seconds_cut(get_time(file_array[i]), get_time_subtr(get_time(row), TIME_BEFORE))
                 seconds_finish_cut = get_seconds_cut(get_time(file_array[i]), get_time_adi(get_time(row), TIME_AFTER))
-                os.system("MP4Box -splitx " + "2" + ":" + "4 " + new_video_path + COMPLETE_NAME + row + ".mp4" + " -out " + new_video_path + HIGHLIGHT_NAME + row + ".mp4")
+                #os.system("MP4Box -splitx " + "2" + ":" + "4 " + new_video_path + COMPLETE_NAME + row + ".mp4" + " -out " + new_video_path + HIGHLIGHT_NAME + row + ".mp4")
             else:
                 i = i+1
